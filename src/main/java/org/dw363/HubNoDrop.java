548a6834ff25a9e08d62a841375bbb1dee121738
dw363 <dw363.frilance@gmail.com>
2024-08-23 15:20:22 +0300

    Версия 1.0.0-OmxRealm

---
+++ src/main/java/org/dw363/HubNoDrop.java
@@ -0,0 +1,24 @@
+package org.dw363;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HubNoDrop extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event) {
+        event.setCancelled(true);
+    }
+}
